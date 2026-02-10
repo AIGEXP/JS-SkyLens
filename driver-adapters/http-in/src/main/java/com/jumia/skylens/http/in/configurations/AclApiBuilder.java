eefd0c1711df48397751d6aab30057eea107ca23
jsolferreira <jsolferreira@gmail.com>
2026-02-10 10:30:13 +0000

    JMIA-6917 - I want to have a flexible ACL permission check

--- driver-adapters/http-in/src/main/java/com/jumia/skylens/http/in/configurations/AclApiBuilder.java
+++ driver-adapters/http-in/src/main/java/com/jumia/skylens/http/in/configurations/AclApiBuilder.java
@@ -2,6 +2,7 @@ package com.jumia.skylens.http.in.configurations;
 
 import com.jumia.skylens.http.in.acl.AuthInstances;
 import com.jumia.skylens.http.in.acl.authentication.UserAuthenticationType;
+import com.jumia.skylens.http.in.configurations.properties.AclCacheProperties;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,11 @@ import org.springframework.stereotype.Component;
 import pt.jumia.services.acl.lib.AclConnectApiClient;
 import pt.jumia.services.acl.lib.AclConnectApiClientBuilder;
 import pt.jumia.services.acl.lib.CacheStrategy;
+import pt.jumia.services.acl.lib.cache.InMemoryCacheStrategy;
 import pt.jumia.services.acl.lib.client.authorization.HierarchicalAuthorizationClient;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -23,7 +26,7 @@ public class AclApiBuilder {
 
     private final AuthInstances authInstances;
 
-    private final CacheStrategy cacheStrategy;
+    private final AclCacheProperties aclCacheProperties;
 
     public AclConnectApiClient<HierarchicalAuthorizationClient> buildAclConnectApiClient(final String applicationCode) {
 
@@ -39,7 +42,7 @@ public class AclApiBuilder {
         Arrays.stream(UserAuthenticationType.values())
                 .map(authInstances.getAuthInstances()::get)
                 .forEach(authInstance -> aclConnectApiClientBuilder.addAclInstance(authInstance.getAclInstance(),
-                        authInstance.isDefault()));
+                                                                                   authInstance.isDefault()));
     }
 
     private AclConnectApiClientBuilder getAclConnectApiClientBuilder(final String applicationCode) {
@@ -47,6 +50,17 @@ public class AclApiBuilder {
         return new AclConnectApiClientBuilder()
                 .setApplicationCode(applicationCode)
                 .setLogger(new AclLogger())
-                .setCacheStrategy(cacheStrategy);
+                .setCacheStrategy(cacheStrategy());
+    }
+
+    private CacheStrategy cacheStrategy() {
+
+        if (aclCacheProperties == null) {
+
+            return InMemoryCacheStrategy.newInstance(1, TimeUnit.HOURS);
+        }
+
+        return InMemoryCacheStrategy.newInstance(aclCacheProperties.expireDuration().toMillis(),
+                                                 TimeUnit.MILLISECONDS);
     }
 }
