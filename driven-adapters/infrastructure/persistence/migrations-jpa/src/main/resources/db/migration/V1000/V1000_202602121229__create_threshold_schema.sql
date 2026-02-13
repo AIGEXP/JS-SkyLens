6f51058d2ee9affc9c20a556f15c7b301ddf98d5
philliplima <phillip.lima@jumia.com>
2026-02-13 11:13:58 +0000

    JMIA-7193 - threshold migration

--- driven-adapters/infrastructure/persistence/migrations-jpa/src/main/resources/db/migration/V1000/V1000_202602121229__create_threshold_schema.sql
+++ driven-adapters/infrastructure/persistence/migrations-jpa/src/main/resources/db/migration/V1000/V1000_202602121229__create_threshold_schema.sql
@@ -1,11 +1,15 @@
+CREATE TYPE REPORT_TYPE AS ENUM (
+    'SUCCESS_RATE',
+    'LOSS_RATE'
+    );
+
 CREATE TABLE network_threshold (
 
-    report_name     VARCHAR(255)                NOT NULL,
-    network         VARCHAR(32)                 NOT NULL,
+    report_type     REPORT_TYPE                 NOT NULL,
+    network         VARCHAR(2)                  NOT NULL,
     threshold       NUMERIC(3,2)                NOT NULL,
-    created_at      TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMP WITH TIME ZONE    DEFAULT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (report_name, network),
+    PRIMARY KEY (report_type, network),
     CONSTRAINT chk_threshold_range CHECK (threshold >= 0 AND threshold <= 1)
 );
