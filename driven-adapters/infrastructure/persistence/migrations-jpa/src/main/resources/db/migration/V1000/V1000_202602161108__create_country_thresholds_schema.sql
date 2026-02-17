ee4e1c5fcff5be68b379173963438be1bd1a176d
philliplima <phillip.lima@jumia.com>
2026-02-17 09:32:49 +0000

    JMIA-7193 - rename network to country in migration

--- driven-adapters/infrastructure/persistence/migrations-jpa/src/main/resources/db/migration/V1000/V1000_202602161108__create_country_thresholds_schema.sql
+++ driven-adapters/infrastructure/persistence/migrations-jpa/src/main/resources/db/migration/V1000/V1000_202602161108__create_country_thresholds_schema.sql
@@ -3,13 +3,13 @@ CREATE TYPE REPORT_TYPE AS ENUM (
     'LOSS_RATE'
     );
 
-CREATE TABLE network_thresholds (
+CREATE TABLE country_thresholds (
 
     report_type     REPORT_TYPE                 NOT NULL,
-    network         VARCHAR(2)                  NOT NULL,
+    country         VARCHAR(2)                  NOT NULL,
     value           NUMERIC(3,2)                NOT NULL,
     updated_at      TIMESTAMP WITH TIME ZONE    DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (report_type, network),
+    PRIMARY KEY (report_type, country),
     CONSTRAINT chk_threshold_range CHECK (value >= 0 AND value <= 1)
 );
